d1adfc93ced4c31e19c237aa6601751cbc7a4dbd
al alamin <mdabdullahalalaminp@gmail.com>
2017-01-29 16:04:14 +0600

    subdomains working

--- bizzybd/home/views.py
+++ bizzybd/home/views.py
@@ -8,8 +8,11 @@ class IndexView(View):
     template_name = 'home/index.html'
 
     def get(self, request, *args, **kwargs):
-
-        return render(request, self.template_name)
+        title = request.get_host()
+        context = {
+            'title': title
+        }
+        return render(request, self.template_name, context)
 
     # def post(self, request, *args, **kwargs):
     #     form = self.form(request.POST)
