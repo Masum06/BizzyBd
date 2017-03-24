8fe3d799342e19e273b614a4080713ad97cc6103
Md Mahmudul Hasan <Md Mahmudul Hasan>
2017-03-24 09:37:19 +0600

    Fixed edit_mode urls problem

--- bizzybd/special_subdomain/urls.py
+++ bizzybd/special_subdomain/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from special_subdomain import views
 
 urlpatterns = [
-    url(r'^', views.TeacherDemoView.as_view(), name='teacher_demo'),
+    url(r'^$', views.TeacherDemoView.as_view(), name='teacher_demo'),
     url(r'^edit/$', views.TeacherDemoEditView.as_view(), name='teacher_demo-edit'),
 ]
